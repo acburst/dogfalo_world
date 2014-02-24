a0106da732f0f971e2057049aacc4f3a9a396bf9
Chang Alan <alanchan@andrew.cmu.edu>
2014-02-23 19:43:59 -0500

    articles branch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ActiveRecord::Base
+
+	# relationships
 	belongs_to :category
 
+	# validations
 	validates_presence_of :title
 	validates_presence_of :content
 
+	# scopes
 	scope :active, where('active = ?', true)
   scope :alphabetical, order('title')
 
